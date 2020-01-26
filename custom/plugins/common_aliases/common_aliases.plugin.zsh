49dcb5cd5004e2e07ddbeda7f8c3ba17ebcf3210
stiles69 <brett.salemink@gmail.com>
2020-01-25 21:57:57 -0600

    Update

--- custom/plugins/common_aliases/common_aliases.plugin.zsh
+++ custom/plugins/common_aliases/common_aliases.plugin.zsh
@@ -1,6 +1,40 @@
-#======= COMMON ALIASES ADDED BY BRETT SALEMINK ======
+# ************** UNIVERSAL ALIASES **************
+
+# --------------- Referenced from --------------
+# http:/LDP/abs/html/sample-bashrc.html
+
+#----------- Get ls and ll aliases ----------
+alias llh='cat $ZSH_CUSTOM/aliases-common.zsh'
+alias lsh='cat $ZSH_CUSTOM/aliases-common.zsh'
+
+#-------------------------------------------------------------
+# The 'ls' family (this assumes you use a recent GNU ls).
+#-------------------------------------------------------------
+# Add colors for filetype and  human-readable sizes by default on 'ls':
+alias ls='ls -h --color'   #  Human Readable
+alias lx='ls -lXB'         #  Sort by extension.
+alias lk='ls -lSr'         #  Sort by size, biggest last.
+alias lt='ls -ltr'         #  Sort by date, most recent last.
+alias lc='ls -ltcr'        #  Sort by/show change time,most recent last.
+alias lu='ls -ltur'        #  Sort by/show access time,most recent last.
+
+# The ubiquitous 'll': directories first, with alphanumeric sorting:
+alias ll="ls -lv --group-directories-first"
+alias lm='ll |more'        #  Pipe through 'more'
+alias lr='ll -R'           #  Recursive ls.
+alias la='ll -A'           #  Show hidden files.
+alias tree='tree -Csuh'    #  Nice alternative to 'recursive ls' ...
+
+# Pretty-print of some PATH variables:
+alias path='echo -e ${PATH//:/\\n}'
+alias libpath='echo -e ${LD_LIBRARY_PATH//:/\\n}'
+
+# ************** UNIVERSAL ALIASES **************
+
+# ======= COMMON ALIASES ADDED BY STILES =======
 
 alias bb="bleachbit --shred"
+alias bin="cd $HOME/GitLab/bin;wait;clear"
 alias c='clear'
 alias cd-b='cd ~/GitLab/bin'
 alias code="/usr/share/code/code"
@@ -13,23 +47,29 @@ alias ipe='curl ipinfo.io/ip'
 alias ld='ls'
 alias ohmyzsh="cd ~/.oh-my-zsh"
 alias reboot='systemctl shutdown now -r'
+alias podman='sudo podman'
+alias mkdir='mkdir -p'
 alias sourcez='source $HOME/.zshrc'
 alias untar='tar -zxvf '
 alias wipe="wipe -rcf"
 alias wget='wget -c '
+alias visudo='EDITOR=nano visudo'
 alias zshrc="vim ~/.zshrc"
 
-#==== TESTING ====
-alias bin="cd /home/brettsalemink/GitLab/bin"
-#==== FUNCTIONS ====
-function tsh { ssh -X $1 -t "tmux -CC attach -t $2 || tmux -CC new -s $2"; } 
+# ====== END COMMON ALIASES ADDED BY STILES =====
 
-function aliases_show ()
-{
-    cat $ZSH_CUSTOM/plugins/common_aliases/common_aliases.plugin.zsh | grep alias
-    wait
-}
+# ================= TESTING =====================
 
+# --------  ALLOW REMOTE EXECUTION ------------
+alias ssh-remote='ssh -t -o StrictHostKeyChecking=no '
+
+
+# =============== END TESTING ==================
+
+
+# ================ FUNCTIONS ===================
+
+# Extracts different archives
 function extractor ()
 {
     if [ -f $1 ] ; then
@@ -63,46 +103,23 @@ function extractor ()
     fi
 }
 
-#== PODMAN ==
-alias podman='sudo podman'
 
-#=== SET DEFAULT EDITOR FOR VISUDO ====
-alias visudo='EDITOR=nano visudo'
+# =============== END FUNCTIONS =================
 
-#============ ALLOW REMOTE EXECUTION =======
-alias ssh-remote='ssh -t -o StrictHostKeyChecking=no '
 
-#========= HUMAN READABLE OUPUT =========
-#alias ls='ls -lash'
+# Shows Aliases in this plugin
+function aliases_show ()
+{
+    cat $ZSH_CUSTOM/plugins/common_aliases/common_aliases.plugin.zsh | grep alias
+    wait
+}
 
-# -> Prevents accidentally clobbering files.
-alias mkdir='mkdir -p'
+# Tmux helper
+function tsh ()
+{ 
+    ssh -X $1 -t "tmux -CC attach -t $2" || "tmux -CC new -s $2"; 
+} 
 
 #----	Dont have to add --progress to rsync
-alias rsync='rsync --progress'
-#====== Referenced from http:/LDP/abs/html/sample-bashrc.html
-#----------- Get ls and ll aliases ----------
-alias llh='cat $ZSH_CUSTOM/aliases-common.zsh'
-alias lsh='cat $ZSH_CUSTOM/aliases-common.zsh'
+#alias rsync='rsync --progress'
 
-#-------------------------------------------------------------
-# The 'ls' family (this assumes you use a recent GNU ls).
-#-------------------------------------------------------------
-# Add colors for filetype and  human-readable sizes by default on 'ls':
-alias ls='ls -h --color'
-alias lx='ls -lXB'         #  Sort by extension.
-alias lk='ls -lSr'         #  Sort by size, biggest last.
-alias lt='ls -ltr'         #  Sort by date, most recent last.
-alias lc='ls -ltcr'        #  Sort by/show change time,most recent last.
-alias lu='ls -ltur'        #  Sort by/show access time,most recent last.
-
-# The ubiquitous 'll': directories first, with alphanumeric sorting:
-alias ll="ls -lv --group-directories-first"
-alias lm='ll |more'        #  Pipe through 'more'
-alias lr='ll -R'           #  Recursive ls.
-alias la='ll -A'           #  Show hidden files.
-alias tree='tree -Csuh'    #  Nice alternative to 'recursive ls' ...
-
-# Pretty-print of some PATH variables:
-alias path='echo -e ${PATH//:/\\n}'
-alias libpath='echo -e ${LD_LIBRARY_PATH//:/\\n}'
